d2616b7f21d8c185137dc1cbc2f5bbd65fa6fa23
techraiders <hello_navneet@hotmail.com>
2016-10-15 11:41:06 +0530

    Stes margin, padding, and colors, and font size in New complaint form

--- www/js/controllers/complaintCtrl.js
+++ www/js/controllers/complaintCtrl.js
@@ -13,13 +13,18 @@
         $scope.subCategories = response;
       });*/
 
-
+    $scope.subCategories = [
+        {id: 1, name: "subc1"},
+        {id: 2, name: "subc3"},
+        {id: 3, name: "subc3"}
+      ];
+    
       $scope.saveComplaint = function (new_complaint) {
         if (new_complaint) {
           complaintsFactory.saveComplaint(new_complaint).then(function(response) {
-          })
           alert("Complaint submitted");
           console.log("Complaint submitted");
+          })
         }
       }
 
