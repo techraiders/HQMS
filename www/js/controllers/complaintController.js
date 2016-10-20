ab25f2e69a4edd73689c7206d2cacc26cbc2bc36
techraiders <hello_navneet@hotmail.com>
2016-10-20 20:22:46 +0530

    Super-popup confirms if user doesn't want to give email to track complaint and submits

--- www/js/controllers/complaintController.js
+++ www/js/controllers/complaintController.js
@@ -19,25 +19,36 @@
 
       function newComplaint(new_complaint) {
         complaintsFactory.saveComplaint(new_complaint).then(function(response) {
-          vm.complaint = {};
+          vm.complaint = undefined;
           showToast("Your complaint was successfully posted", "top", false, 3000);
           $state.go("thankyou");
         });
       }
 
-      vm.saveComplaint = function(new_complaint) {
-        vm.submitted = true;        
+      function ifValidEmail (new_complaint) {
         if (angular.isString(new_complaint.emailId) && new_complaint.emailId) {
+          vm.isEmail = true;
+          return vm.isEmail;
+        }
+      }
+
+      vm.print = function(new_complaint) {
+        console.log(ifValidEmail(new_complaint));
+      }
+
+      vm.saveComplaint = function(new_complaint) {
+        if (ifValidEmail(new_complaint)) {
           newComplaint(new_complaint);
-        } else {
-          if (confirm("Are you sure you want to submit without email address, you won't be able to track the complaint status?")) {
-            newComplaint(new_complaint);
+        }
+        else {
+          if (popup5(new_complaint)) {
+            //newComplaint(new_complaint);
           }
         }
       };
 
 
-        vm.popup5 = function () {
+        function popup5(new_complaint) {
             ionicSuperPopup.show({
               title: "Are you sure?",
               text: "This will do something again!",
@@ -45,11 +56,13 @@
                showCancelButton: true,
                confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, do that thing!",
                cancelButtonText: "No, don't!",
-               closeOnConfirm: false,
+               closeOnConfirm: true,
                closeOnCancel: false },
             function(isConfirm) {
                if (isConfirm) {
-                  ionicSuperPopup.show("You did it!", "You totally just did something!", "success");
+                  //ionicSuperPopup.show("You did it!", "You totally just did something!", "success");
+                  newComplaint(new_complaint);
+                  console.log("you opted not to give email");
                } else {
                   ionicSuperPopup.show("Cancelled", "Pew, you totally didn't do anything!", "error");
                }
