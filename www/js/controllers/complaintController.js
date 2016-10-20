afedcf193bf466d4834ffa64b5b6b426834e9387
techraiders <hello_navneet@hotmail.com>
2016-10-20 12:23:03 +0530

    Confirms if user doesn't want to submit email id, tells s/he won't be able to track the complaint status

--- www/js/controllers/complaintController.js
+++ www/js/controllers/complaintController.js
@@ -13,19 +13,26 @@
         vm.subCategories = response;
       });
  
-      $rootScope.$on("$stateChangeStart", function(event, toState, fromState) {
-        if (!confirm("Are you sure you to navigate away")) {
-          event.preventDefault();
-        }
-      })
+     vm.Confirm = function () {
+      vm.x = confirm("Are you sure you")
+        console.log(vm.x);
+      }
 
-      vm.saveComplaint = function(new_complaint) {
-        vm.submitted = true;
-        if (new_complaint) {
-          complaintsFactory.saveComplaint(new_complaint).then(function(response) {
+      function newComplaint(new_complaint) {
+        complaintsFactory.saveComplaint(new_complaint).then(function(response) {
           vm.complaint = {};
           $state.go("thankyou");
-          })
+        });
+      }
+
+      vm.saveComplaint = function(new_complaint) {
+        vm.submitted = true;        
+        if (angular.isString(new_complaint.emailId) && new_complaint.emailId) {
+          newComplaint(new_complaint);
+        } else {
+          if (confirm("Are you sure you want to submit without email address, you won't be able to track the complaint status?")) {
+            newComplaint(new_complaint);
+          }
         }
       }
   });
