0aa99da4d54aa703d77af3363489bb5e7672531f
techraiders <hello_navneet@hotmail.com>
2016-10-28 18:44:08 +0530

    Adds star based rating form in surveys

--- www/js/controllers/surveysController.js
+++ www/js/controllers/surveysController.js
@@ -7,6 +7,10 @@
         var vm = this;
         vm.surveys = surveys;
 
+        vm.val = function(choice) {
+          console.log(choice);
+        }
+
         vm.sliderQuestions = [
           {
             "id": 9,
@@ -33,6 +37,59 @@
             "question_type": "range",
           }
         ];
+
+        vm.starQuestions = [
+          {
+            "id": 9,
+            "question": "Write here question for star based rating.",
+            "title": "Please rate this service by choosing below starts.",
+            "category": 9,
+            "survey": 1,
+            "question_type": "range",
+            "choices":['1', '2', '3', '4', '5']
+          },{
+            "id": 9,
+            "question": "Write here question for star based rating.",
+            "title": "Please rate this service by choosing below starts.",
+            "category": 9,
+            "survey": 1,
+            "question_type": "range",
+            "choices":['1', '2', '3', '4', '5']
+          },{
+            "id": 9,
+            "question": "Write here question for star based rating.",
+            "title": "Please rate this service by choosing below starts.",
+            "category": 9,
+            "survey": 1,
+            "question_type": "range",
+            "choices":['1', '2', '3', '4', '5']
+          },{
+            "id": 9,
+            "question": "Write here question for star based rating.",
+            "title": "Please rate this service by choosing below starts.",
+            "category": 9,
+            "survey": 1,
+            "question_type": "range",
+            "choices":['1', '2', '3', '4', '5']
+          }
+        ];
+
+        vm.ratingsObject = {
+          iconOn : 'ion-ios-star',
+          iconOff : 'ion-ios-star-outline',
+          iconOnColor: 'rgb(200, 200, 100)',
+          iconOffColor:  'rgb(200, 100, 100)',
+          rating:  2,
+          minRating:1,
+          callback: function(rating) {
+            vm.ratingsCallback(rating);
+          }
+        };
+
+        vm.ratingsCallback = function(rating) {
+          console.log('Selected rating is : ', rating);
+        };
+
         //console.log(vm.sliderQuestions);
         customService.spinner_off();
       });
